17cebed98de15bd84b26aaae13c400656f7e163c
Justin Maure <justin.maure01@stclairconnect.ca>
2018-03-27 21:19:14 -0400

    Created the user class' properties and methods

--- app/src/main/java/com/justinmaure/datenight/Objects/User.java
+++ app/src/main/java/com/justinmaure/datenight/Objects/User.java
@@ -5,6 +5,46 @@ package com.justinmaure.datenight.Objects;
  */
 
 public class User {
-    
+    private String username;
+    private String password;
+    private String email;
+    private Boolean isLoggedIn = false;
 
+    public User(String username, String password, String email){
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Boolean getLoggedInStatus() {
+        return isLoggedIn;
+    }
+
+    public void setLoggedIn(Boolean loggedIn) {
+        isLoggedIn = loggedIn;
+    }
 }
