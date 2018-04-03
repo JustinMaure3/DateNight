d59812f1ca83e3b06198fcec9c990b7e24120416
Justin Maure <justin.maure01@stclairconnect.ca>
2018-04-03 16:46:01 -0400

    Fixed all errors in the custom adapter, refactored it to be customAdapterMyDates i also reworked the myDates card view

---
+++ app/src/main/java/com/justinmaure/datenight/CustomAdapterMyDates.java
@@ -0,0 +1,73 @@
+package com.justinmaure.datenight;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.justinmaure.datenight.Objects.Date;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Max on 3/31/18.
+ */
+
+public class CustomAdapterMyDates extends RecyclerView.Adapter {
+    private ArrayList<Date> dates;
+
+
+    Context context;
+
+    //RecyclerView for "My Dates"
+    public CustomAdapterMyDates(ArrayList<Date> dates) {
+        this.dates = dates;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, final int viewType){
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.pop_dates_recycler_view, parent, false);
+        final CustomViewHolder viewHolder = new CustomViewHolder(view);
+        context = parent.getContext();
+
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        Date date = dates.get(position);
+        ((CustomViewHolder) holder).dateName.setText(date.getDateName());
+    }
+
+    @Override
+    public int getItemCount() {
+        if(dates != null){
+            return dates.size();
+        }
+        return 0;
+    }
+
+    class CustomViewHolder extends RecyclerView.ViewHolder{
+        protected TextView dateName;
+        protected TextView description;
+        protected TextView rating;
+        protected ImageView picture;
+        protected ImageView isPublic;
+
+        public CustomViewHolder(View view){
+            super(view);
+            this.dateName = (TextView) view.findViewById(R.id.name);
+            this.description = (TextView) view.findViewById(R.id.description);
+            this.picture = (ImageView) view.findViewById(R.id.picture);
+            this.rating = (TextView) view.findViewById(R.id.rating);
+            this.isPublic = (ImageView) view.findViewById(R.id.isPublic);
+
+        }
+    }
+
+}
